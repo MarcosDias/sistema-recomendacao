af49ee82735df35ef5b09466ef9658933d3ec752
Marcos Dias <marcos.a.melo.dias@gmail.com>
2014-12-19 16:56:21 -0200

    DocString'

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ ARQUIVOS_TESTE2 = "data/02-test.txt"
 
 
 def start():
+    """
+    Funcao main do programa
+    """
     print "=== Um Sistema de Recomendação baseado em \
 Conteúdo para Sugestão de Notícias Relacionadas ==="
 
@@ -31,15 +34,34 @@ Conteúdo para Sugestão de Notícias Relacionadas ==="
 
 
 def get_key_sort(l):
+    """
+    Funcao auxiliar para o metodo sorted dos lists.
+    Como a lista que sera ordenada tem o formato:
+    [[A, B], [C, D], [E, F], [G, H]]
+    Eh necessario um metodo que retorne qual valor das listas internas sera usado para ordenar
+
+    :param l: lista com o formato desejado para ordenacao
+    :return: retorna a sublista
+    """
     return l[0]
 
 
 def print_arquivo_classe(path, classe):
+    """
+    Printa qual arquiro sera usado como entrada no momento
+    :param path: o arquivo (caminho) que sera usado
+    :param classe: classe do arquivo que esta sendo usado
+    """
     print
     print path, classe
 
 
 def print_list_distancia(list_distancia, quant_loops):
+    """
+    Printa uma lista contendo o arquivo que foi feito o calculo e a distancia em si
+    :param list_distancia: lista de arquivos que foram calculados
+    :param quant_loops: quantidade de arquivos que serao impressos no terminal
+    """
     k = 0
     print '--------------------------'
     print 'Textos mais parecidos'
