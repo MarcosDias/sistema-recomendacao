62b5759131f0aff86a6f3638e9095cd33e5a587a
Marcos Dias <marcos.a.melo.dias@gmail.com>
2014-12-19 15:36:03 -0200

    calculo de distancia feito

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from funcs.funcs import monta_indice_arquivo
+from funcs.funcs import monta_indice_arquivo, calcula_distancia
 
 BASE_CONHECIMENTO1 = "data/01-train.txt"
 ARQUIVOS_TESTE1 = "data/01-test.txt"
@@ -15,8 +15,33 @@ Conteúdo para Sugestão de Notícias Relacionadas ==="
     dic_global = monta_indice_arquivo(BASE_CONHECIMENTO1)
     dic_teste = monta_indice_arquivo(ARQUIVOS_TESTE1)
 
+    for chaves_test in dic_teste:
+        list_distancia = []
+
+        print_arquivo_classe(chaves_test, dic_teste[chaves_test][0])
+
+        for chaves_global in dic_global:
+            if chaves_test != chaves_global:
+                distancia = calcula_distancia(dic_teste[chaves_test], dic_global[chaves_global])
+                list_distancia.append([distancia, chaves_global])
+
+        list_distancia.sort(key=get_key_sort)
+
+        print '--------------------------'
+        print 'Textos mais parecidos'
+        print '--------------------------'
+        for i in list_distancia:
+            print i[1], i[0]
+
+
+def get_key_sort(l):
+    return l[0]
+
+
+def print_arquivo_classe(path, classe):
+    print
+    print path, classe
+
 
 if __name__ == "__main__":
     start()
-
-
